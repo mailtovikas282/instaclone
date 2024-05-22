c55973b27cb4d6a97aaec6b8baf40e4fcf42a2ec
vikas singh <mailtovikas282@gmail.com>
2024-05-22 11:43:34 +0530

    first commit

---
+++ backend/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    
+     mongoUrl: "mongodb+srv://mailtovikas2802:Vikassingh12345@cluster0.rilullf.mongodb.net",
+     Jwt_secret: "aaaa"
+};
---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+import logo from "./logo.svg";
+import React, { createContext, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import Profie from "./components/Profie";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Createpost from "./components/Createpost";
+import { LoginContext } from "./context/LoginContext";
+import Modal from "./components/Modal";
+import UserProfie from "./components/UserProfile";
+import MyFolliwngPost from "./components/MyFollowingPost";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+
+
+function App() {
+  const [userLogin, setUserLogin] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <BrowserRouter>
+      <div className="App">
+      <GoogleOAuthProvider clientId="331233129743-5aqnmpnl7p9tk7lnpu47juvbrdm6d8ti.apps.googleusercontent.com">
+        <LoginContext.Provider value={{ setUserLogin, setModalOpen }}>
+          <Navbar login={userLogin} />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/signin" element={<SignIn />}></Route>
+            <Route exact path="/profile" element={<Profie />}></Route>
+            <Route path="/createPost" element={<Createpost />}></Route>
+            <Route path="/profile/:userid" element={<UserProfie />}></Route>
+            <Route path="/followingpost" element={<MyFolliwngPost />}></Route>
+          </Routes>
+          <ToastContainer theme="dark" />
+
+          {modalOpen && <Modal setModalOpen={setModalOpen}></Modal>}
+        </LoginContext.Provider>
+      </GoogleOAuthProvider>;
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
